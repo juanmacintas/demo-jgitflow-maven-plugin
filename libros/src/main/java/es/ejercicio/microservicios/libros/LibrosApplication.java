477036e752b3e888dc80ba9a7a3b8a942a485978
Cintas Peña, Juan M <juan.m.cintas.pena@accenture.com>
2019-10-23 13:01:29 +0200

    version inicial

---
+++ libros/src/main/java/es/ejercicio/microservicios/libros/LibrosApplication.java
@@ -0,0 +1,12 @@
+package es.ejercicio.microservicios.libros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrosApplication.class, args);
+	}
+}
